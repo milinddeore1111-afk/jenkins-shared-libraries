48f1a7d8db3e379cc68523a25be7c8e578992da9
milinddeore1111-afk <milinddeore1111@gmail.com>
2026-04-06 01:52:29 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -9,3 +9,4 @@ sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
 sh "docker image tag notes-app:latest ${env.dockerHubUser}/notes-app:latest"
 sh "docker push ${env.dockerHubUser}/notes-app:latest"
 }  
+}
